d905037b3f6539984bddaf0bb8a8c2620055a7df
Lorenzo Maiuri <maiurilorenzo@gmail.com>
2026-06-02 20:18:09 +0200

    feat: delete old images in registry

--- infra/modules/artifact_registry/main.tf
+++ infra/modules/artifact_registry/main.tf
@@ -4,4 +4,22 @@ resource "google_artifact_registry_repository" "backend" {
   location      = var.region
   project       = var.project_id
   description   = "Docker images for lorenzobot backend"
+
+  cleanup_policy_dry_run = false
+
+  cleanup_policies {
+    id     = "delete-old-versions"
+    action = "DELETE"
+    condition {
+      tag_state = "ANY"
+    }
+  }
+
+  cleanup_policies {
+    id     = "keep-latest-image"
+    action = "KEEP"
+    most_recent_versions {
+      keep_count = 1
+    }
+  }
 }
