42f612d0ba410a917436d2bb77e3d9e028ecc7f5
Lorenzo Maiuri <maiurilorenzo@gmail.com>
2026-06-03 15:06:31 +0200

    fix: ignore missing tag for image

--- infra/modules/cloudrun/main.tf
+++ infra/modules/cloudrun/main.tf
@@ -64,6 +64,10 @@ resource "google_cloud_run_v2_service" "backend" {
   }
 
   depends_on = [var.secrets_ready]
+
+  lifecycle {
+    ignore_changes = [template[0].containers[0].image]
+  }
 }
 
 # Allow unauthenticated invocations (public API)
